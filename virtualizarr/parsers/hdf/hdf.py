4f1ce0c27753a04535546c507b8e75bbbbe0b845
Max Jones <14077947+maxrjones@users.noreply.github.com>
2026-04-12 21:34:55 -0700

    Fix fillvalue for bytes

--- virtualizarr/parsers/hdf/hdf.py
+++ virtualizarr/parsers/hdf/hdf.py
@@ -77,7 +77,8 @@ def _construct_manifest_array(
 
     codec_configs = [zarr_codec_config_to_v3(codec.get_config()) for codec in codecs]
 
-    fill_value = dataset.fillvalue.item()
+    fv = dataset.fillvalue
+    fill_value = fv.item() if hasattr(fv, "item") else fv
     dims = tuple(_dataset_dims(dataset, group=group))
     metadata = create_v3_array_metadata(
         shape=dataset.shape,
